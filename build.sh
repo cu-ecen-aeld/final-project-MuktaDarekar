3a6ef61d323d1b5b83e20ce6c261d1a12d3d55b4
MuktaDarekar <mukta.darekar@colorado.edu>
2021-11-07 17:54:30 -0700

    Initial Bringup Yocto image - RaspberryPi

--- build.sh
+++ build.sh
@@ -22,15 +22,30 @@ else
 	echo "${CONFLINE} already exists in the local.conf file"
 fi
 
+#Create image of the type rpi-sdimg
+IMAGE="IMAGE_FSTYPES = \"wic.bz2\""
 
-bitbake-layers show-layers | grep "meta-aesd" > /dev/null
-layer_info=$?
+cat conf/local.conf | grep "${IMAGE}" > /dev/null
+local_image_info=$?
 
-if [ $layer_info -ne 0 ];then
-	echo "Adding meta-aesd layer"
-	bitbake-layers add-layer ../meta-aesd
+if [ $local_image_info -ne 0 ];then 
+    echo "Append ${IMAGE} in the local.conf file"
+	echo ${IMAGE} >> conf/local.conf
 else
-	echo "meta-aesd layer already exists"
+	echo "${IMAGE} already exists in the local.conf file"
+fi
+
+#Set GPU memory as minimum
+MEMORY="GPU_MEM = \"16\""
+
+cat conf/local.conf | grep "${MEMORY}" > /dev/null
+local_memory_info=$?
+
+if [ $local_memory_info -ne 0 ];then
+    echo "Append ${MEMORY} in the local.conf file"
+	echo ${MEMORY} >> conf/local.conf
+else
+	echo "${MEMORY} already exists in the local.conf file"
 fi
 
 
